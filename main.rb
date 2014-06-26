4430cb4eb468b761924e9100592f3cebe1f80e64
Daniel Romero <dromveg@gmail.com>
2014-06-26 17:17:46 +0200

    Removed the main.rb file that was created for testing purposes

--- main.rb
+++
@@ -1,9 +0,0 @@
-require './lib/lyrics_finder'
-
-require 'objspace'
-
-fetcher = LyricsFinder::Fetcher.new(:azlyrics,:song_lyrics,:lyrics_wikia)
-puts song_lyric = fetcher.search("american authors", "best day of my life")
-
-puts "CLASS: #{song_lyric.class}"
-puts "MEMSIZE: #{ObjectSpace.memsize_of(song_lyric)}"
