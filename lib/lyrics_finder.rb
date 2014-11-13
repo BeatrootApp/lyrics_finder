85be0cba124ef4e7eee70f72d651009ab072ee0d
Daniel Romero Vega <dromveg@gmail.com>
2014-11-13 11:28:20 +0100

    Make perform_request private and change dependencies

--- lib/lyrics_finder.rb
+++ lib/lyrics_finder.rb
@@ -1,21 +1,31 @@
-require_relative 'lyrics_finder/dependencies'
+require 'open-uri'
+require 'nokogiri'
+require 'active_support/core_ext'
+require 'i18n'
+I18n.enforce_available_locales = false
+require 'lyrics_finder/version'
+require 'lyrics_finder/song'
+require 'lyrics_finder/providers'
+
 
 module LyricsFinder
   def self.search(author, title)
     Providers.list.each do |provider|
       url = Providers.provider_url_for_song(provider, Song.new(author, title))
       data = perform_request(url)
-      result = Providers.extract_lyric_from_data(data) if data
+      result = Providers.extract_lyric_from_data(data) unless data.nil?
       return result unless result.nil?
     end
-    return nil # because song cannot be found.
+    return nil # song not found
   end
 
-  def self.perform_request(url)
-    begin
-      open(url)
-    rescue Exception => ex
-      # puts "ERROR: " + ex.message
+  private
+
+    def self.perform_request(url)
+      begin
+        open(url)
+      rescue Exception => ex
+        # puts "ERROR: " + ex.message
+      end
     end
-  end
 end
--- lib/lyrics_finder/dependencies.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'active_support/core_ext'
-require 'i18n'
-I18n.enforce_available_locales = false
-
-require_relative 'version'
-require_relative 'song'
-require_relative 'providers'
